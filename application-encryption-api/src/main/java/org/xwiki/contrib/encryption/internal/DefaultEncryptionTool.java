72e3b0c03cfb818025153ab0dd39f1b7c8355b49
4F2E4A2E <4F2E4A2E@users.noreply.github.com>
2015-03-25 18:00:45 +0100

    * Setting XWIKI Version to 6.4.2 * Removed some deprecated calls, still many to go * FIXING Bug ENCRYPTAPP-1 and refactoring decryption rights check * Added new developer to pom.xml * Added issue management information

--- application-encryption-api/src/main/java/org/xwiki/contrib/encryption/internal/DefaultEncryptionTool.java
+++ application-encryption-api/src/main/java/org/xwiki/contrib/encryption/internal/DefaultEncryptionTool.java
@@ -23,9 +23,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.security.KeyStore;
-import java.util.Scanner;
 
-import com.xpn.xwiki.XWikiContext;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
@@ -34,17 +38,13 @@ import org.xwiki.component.annotation.Component;
 import org.xwiki.context.Execution;
 import org.xwiki.context.ExecutionContext;
 import org.xwiki.contrib.encryption.EncryptionTool;
-import org.xwiki.environment.*;
+import org.xwiki.environment.Environment;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
 import org.xwiki.model.reference.EntityReferenceSerializer;
+import org.xwiki.security.authorization.AuthorizationManager;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.inject.Singleton;
-import javax.inject.Inject;
+import com.xpn.xwiki.XWikiContext;
 
 /**
  * Implementation of a <tt>Encryption Tool</tt> component.
@@ -55,38 +55,38 @@ public class DefaultEncryptionTool implements EncryptionTool
 {
     /** The file where we store the encryption key. */
     private static final String ENCRYPTION_FILE_NAME = "EncryptionToolKey.txt";
-    
+
     private static final String KEYSTORE_PASSWORD = "";
-    
+
     private static final String ENCRYPTION_KEY_PROTECTION = "";
-    
-    @Inject 
+
+    @Inject
     private Environment environment;
-    
+
     @Inject
     private Logger logger;
-    
+
     /** Provides access to documents. Injected by the Component Manager. */
     @Inject
     private DocumentAccessBridge documentAccessBridge;
-    
+
     @Inject
     private DocumentReferenceResolver<String> resolver;
-    
+
     /** Provides access to the request context. Injected by the Component Manager. */
     @Inject
     private Execution execution;
-    
+
     /**
      * Reference string serializer.
      */
     @Inject
     protected EntityReferenceSerializer<String> stringSerializer;
-    
+
     @Override
     public String encrypt(String clearText)
     {
-        try{
+        try {
             logger.debug("Encrypt started");
             Cipher c1 = Cipher.getInstance("AES");
             SecretKeySpec key = this.getKey();
@@ -95,14 +95,12 @@ public class DefaultEncryptionTool implements EncryptionTool
             clearTextBytes = clearText.getBytes();
             byte[] encryptedText = c1.doFinal(clearTextBytes);
             return new String(Base64.encodeBase64(encryptedText));
-            }
-            catch(Exception e)
-            {
-                logger.warn("Exception encountered while trying to perform encryption : " + e.getMessage());
-                return null;
-            }
+        } catch (Exception e) {
+            logger.warn("Exception encountered while trying to perform encryption : " + e.getMessage());
+            return null;
+        }
     }
-    
+
     private String decrypt(String encryptedText)
     {
         try {
@@ -120,133 +118,145 @@ public class DefaultEncryptionTool implements EncryptionTool
             return null;
         }
     }
-    
+
     @Override
     public String display(String encryptedText, boolean viewRight)
     {
-        if(viewRight)
+        if (viewRight)
             return decrypt(encryptedText);
         else
             return null;
     }
-    
+
     @Override
     public boolean hasDecryptionRight()
     {
-        try{
+        boolean result = false;
+        try {
             ExecutionContext context = this.execution.getContext();
             XWikiContext xwikiContext = (XWikiContext) context.getProperty("xwikicontext");
             DocumentReference userRef = this.documentAccessBridge.getCurrentUserReference();
-            logger.warn("Proper wiki : " + documentAccessBridge.getCurrentDocumentReference().getWikiReference().toString());
-            String wiki = documentAccessBridge.getCurrentWiki();
-            logger.warn("WikiName : " + wiki);
-            if(xwikiContext.getWiki().getRightService().hasAccessLevel("admin", stringSerializer.serialize(userRef), "XWiki.XWikiPreferences", xwikiContext))
-            {
+            logger.debug("Proper wiki : "
+                + documentAccessBridge.getCurrentDocumentReference().getWikiReference().toString());
+            String wiki = documentAccessBridge.getCurrentDocumentReference().getWikiReference().getName();
+            logger.debug("WikiName : " + wiki);
+            if (xwikiContext.getWiki().getRightService()
+                .hasAccessLevel("admin", stringSerializer.serialize(userRef), "XWiki.XWikiPreferences", xwikiContext)) {
                 logger.debug("Current user has programming rights on this wiki.");
-                return true ;
+                result = true;
+                return result;
             }
             String adminPage = wiki + ":Encryption.Administration";
             DocumentReference docRef = resolver.resolve(adminPage);
-            String rightsClass = wiki+":Encryption.DecryptionRightClass";
+            String rightsClass = wiki + ":Encryption.DecryptionRightClass";
             DocumentReference classRef = resolver.resolve(rightsClass);
+
+            /**
+             * Content example of the currentUser: xwiki:XWiki.username
+             */
             String currentUser = userRef.toString();
+
             int nb = this.documentAccessBridge.getObjectNumber(docRef, classRef, "type", "Wiki");
-            if(nb >= 0)
-            {
+            if (nb >= 0) {
                 Object authorizedUsers = this.documentAccessBridge.getProperty(docRef, classRef, nb, "authorizedUsers");
-                Scanner scan = new Scanner(authorizedUsers.toString()).useDelimiter(",");
-                while(scan.hasNext())
-                {
-                    if(scan.next().equals(currentUser))
-                    {
-                        logger.debug("Current user has decryption rights on the whole wiki");
-                        return true ;
-                    }
+                String[] users = authorizedUsers.toString().split(",");
+                if (hashDecryptionRights(currentUser, users)) {
+                    logger.debug("Current user has decryption rights on the whole wiki");
+                    result = true;
+                    return result;
                 }
             }
             /* Now let's check rights on spaces */
-            String space = this.documentAccessBridge.getCurrentDocumentReference().getSpaceReferences().get(0).getName();
+            String space =
+                this.documentAccessBridge.getCurrentDocumentReference().getSpaceReferences().get(0).getName();
             nb = this.documentAccessBridge.getObjectNumber(docRef, classRef, "name", space);
-            if(nb >= 0)
-            {
-                Object authorizedUsersSpace = this.documentAccessBridge.getProperty(docRef, classRef, nb, "authorizedUsers");
-                Scanner scanSpace = new Scanner(authorizedUsersSpace.toString()).useDelimiter(",");
-                while(scanSpace.hasNext())
-                {
-                    if(scanSpace.next().equals(currentUser))
-                    {
-                        logger.debug("Current user has decryption rights on this space");
-                        return true ;
-                    }
+            if (nb >= 0) {
+                Object authorizedUsersSpace =
+                    this.documentAccessBridge.getProperty(docRef, classRef, nb, "authorizedUsers");
+                String[] users = authorizedUsersSpace.toString().split(",");
+                if (hashDecryptionRights(currentUser, users)) {
+                    logger.debug("Current user has decryption rights on this space");
+                    result = true;
+                    return result;
                 }
             }
             String page = this.documentAccessBridge.getCurrentDocumentReference().getName();
-            page = space + "." + page; 
+            page = space + "." + page;
             nb = this.documentAccessBridge.getObjectNumber(docRef, classRef, "name", page);
-            if(nb >= 0)
-            {
+            if (nb >= 0) {
                 logger.debug("Object found");
-                Object authorizedUsersPage = this.documentAccessBridge.getProperty(docRef, classRef, nb, "authorizedUsers");
+                Object authorizedUsersPage =
+                    this.documentAccessBridge.getProperty(docRef, classRef, nb, "authorizedUsers");
                 logger.debug("Authorized users : " + authorizedUsersPage.toString());
-                Scanner scanPage = new Scanner(authorizedUsersPage.toString()).useDelimiter(",");
-                while(scanPage.hasNext())
-                {
-                    if(scanPage.next().equals(currentUser))
-                    {
-                        logger.debug("Current user has decryption rights on this page");
-                        return true ;
-                    }
+
+                String[] users = authorizedUsersPage.toString().split(",");
+                if (hashDecryptionRights(currentUser, users)) {
+                    logger.debug("Current user has decryption rights on this page");
+                    result = true;
+                    return result;
                 }
             }
-            return false ;
-        }
-        catch(Exception e)
-        {
+            result = false;
+            return result;
+        } catch (Exception e) {
             logger.warn("Unable to verify decryption rights");
-            return false;
+            result = false;
+            return result;
         }
     }
-    
+
+    /**
+     * @param currentUser
+     * @param decryptionRightUsers
+     * @return true if the current user name equals one of the decryption right users.
+     */
+    private boolean hashDecryptionRights(String currentUser, String[] decryptionRightUsers)
+    {
+        boolean result = false;
+
+        currentUser = currentUser.substring(currentUser.lastIndexOf(".") + 1);
+        for (String tmpUser : decryptionRightUsers) {
+            tmpUser = tmpUser.substring(tmpUser.lastIndexOf(".") + 1);
+            if (currentUser.equalsIgnoreCase(tmpUser)) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
     private SecretKeySpec generateRandomKey()
     {
-        try{
+        try {
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(128);
             SecretKey key = keyGenerator.generateKey();
             return (SecretKeySpec) key;
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             logger.warn("Exception encountered while generating the encryption key : " + e.getMessage());
             return null;
         }
     }
-    
+
     private SecretKeySpec getKey()
     {
-        try
-        {
+        try {
             KeyStore ks = KeyStore.getInstance("JCEKS");
             char[] password = KEYSTORE_PASSWORD.toCharArray();
             File file = this.getEncryptionFile();
-            if(!file.exists())
-            {
+            if (!file.exists()) {
                 logger.warn("The encryption file doesn't exist yet");
                 ks = initiateStore(ks, password, file);
-            }
-            else
+            } else
                 ks.load(new FileInputStream(file), password);
             return retrieveEncryptionKey(ks);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             logger.warn("Cannot retrieve encryption key : " + e.getMessage());
             return null;
         }
     }
-    
+
     /**
-     * 
      * @param ks Keystore
      * @param password Password of the keystore
      * @param file File where the keystore is
@@ -255,9 +265,8 @@ public class DefaultEncryptionTool implements EncryptionTool
      */
     private synchronized KeyStore initiateStore(KeyStore ks, char[] password, File file) throws Exception
     {
-        if(file.exists())
-        {
-            //If the file already exists, it means another thread created it in the meanwhile
+        if (file.exists()) {
+            // If the file already exists, it means another thread created it in the meanwhile
             ks.load(new FileInputStream(file), password);
             return ks;
         }
@@ -266,68 +275,61 @@ public class DefaultEncryptionTool implements EncryptionTool
         storeEncryptionKey(ks);
         return ks;
     }
-    
+
     /**
-     * Get the file where the encryptionKey is supposed to be stored.  
+     * Get the file where the encryptionKey is supposed to be stored.
+     * 
      * @return The file where the key is to be stored.
      */
     private File getEncryptionFile()
     {
         File permDir = environment.getPermanentDirectory();
         String path = permDir.getAbsolutePath() + File.separator + ENCRYPTION_FILE_NAME;
-        File encryptionFile = new File(path) ;
-        return encryptionFile ;
+        File encryptionFile = new File(path);
+        return encryptionFile;
     }
-    
+
     /**
      * Store the encryption key.
+     * 
      * @param ks Keystore where the key should be stored
      */
     private void storeEncryptionKey(KeyStore ks)
     {
-        try
-        {
+        try {
             logger.debug("Start storing password");
             String storePassword = KEYSTORE_PASSWORD;
             String protection = ENCRYPTION_KEY_PROTECTION;
             SecretKeySpec key = generateRandomKey();
             logger.debug("Encryption key generated : " + key);
-            KeyStore.SecretKeyEntry skEntry =
-                new KeyStore.SecretKeyEntry(key);
-            ks.setEntry("encryptionKey", skEntry, 
-                new KeyStore.PasswordProtection(protection.toCharArray()));
+            KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(key);
+            ks.setEntry("encryptionKey", skEntry, new KeyStore.PasswordProtection(protection.toCharArray()));
             File file = this.getEncryptionFile();
-            if(!file.exists())
-            {
+            if (!file.exists()) {
                 file.createNewFile();
             }
             FileOutputStream fos = new FileOutputStream(file);
             ks.store(fos, storePassword.toCharArray());
             logger.debug("Finish storing encryption key");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             logger.warn("Exception encountered while trying to store the key : " + e.getMessage());
         }
     }
-    
+
     private SecretKeySpec retrieveEncryptionKey(KeyStore ks)
     {
         String protection = ENCRYPTION_KEY_PROTECTION;
-        try
-        {
+        try {
             logger.debug("Start retrieving password");
-            KeyStore.SecretKeyEntry pkEntry = (KeyStore.SecretKeyEntry)
-                ks.getEntry("encryptionKey", new KeyStore.PasswordProtection(protection.toCharArray()));
+            KeyStore.SecretKeyEntry pkEntry =
+                (KeyStore.SecretKeyEntry) ks.getEntry("encryptionKey",
+                    new KeyStore.PasswordProtection(protection.toCharArray()));
             SecretKeySpec mySecretKey = (SecretKeySpec) pkEntry.getSecretKey();
             return mySecretKey;
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             logger.warn("Exception encountered while trying to retrieve the password : " + e.getMessage());
             return null;
         }
     }
-    
-}
 
+}
